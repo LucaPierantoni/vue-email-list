21632674bc52633f5931f48c1ca92f7ef181a0ff
LucaPierantoni <luca.pier99@gmail.com>
2024-05-28 16:30:10 +0200

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            emails: [],
+            toggle: true
+        }
+
+    },
+    methods: {
+        genEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+                    if(resp.data.success) {
+                        this.emails.push(resp.data.response);
+                    }
+                })};
+            },
+        loading(){
+            this.toggle = !this.toggle
+        }
+    },
+    mounted() {
+
+
+    }
+}).mount('#app')
